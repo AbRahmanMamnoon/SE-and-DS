a2e9bbcdf8c777d2ede872ff4afb3cf62c9e161d
A Rahman Mamnoon <abdulrahmanmamnoon82@gmail.com>
2024-02-23 23:45:04 +0430

    Factory design pattern

---
+++ src/com/java9s/designpattern/factory/IPhone.java
@@ -0,0 +1,12 @@
+package com.java9s.designpattern.factory;
+
+public class IPhone implements Mobile{
+	private int ramSize;
+	private String processor;
+	private String GPU;
+	public IPhone(int ramSize, String processor, String GPU){
+		this.ramSize = ramSize;
+		this.processor = processor;
+		this.GPU = GPU;
+	}
+}
---
+++ src/com/java9s/designpattern/factory/Sony.java
@@ -0,0 +1,11 @@
+package com.java9s.designpattern.factory;
+
+public class Sony implements Mobile{
+	private int ramSize;
+	private String processor;
+	
+	public Sony(int ramSize, String processor){
+		this.ramSize = ramSize;
+		this.processor = processor;
+	}
+}
